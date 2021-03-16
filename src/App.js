8a1acb6435edae2cd2cf0bcdbdae92cada30a8b5
Toby <toby.nguyen.goldenowl@gmail.com>
2021-03-16 17:52:31 +0700

    feat:clicked img to check

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoItem from './Components/Todoitem';
 
 class App extends Component {
   constructor() {
     super();
-    this.toDoItemsList = [
-      { item: 'da bong', isComplete: true },
-      { item: 'xem phim', isComplete: false },
-      { item: 'choi the thao', isComplete: false },
-    ];
+    this.state = {
+      // eslint-disable-next-line react/no-unused-state
+      toDoItemsList: [
+        { item: 'da bong', isComplete: true },
+        { item: 'xem phim', isComplete: true },
+        { item: 'choi the thao', isComplete: false },
+      ],
+    };
   }
 
+  /* eslint-disable */
+  clickItem(item) {
+    // this.toDoItemsList.isComplete = !this.toDoItemsList.isComplete;
+    return (_event) => {
+      const { isComplete } = item;
+      const { toDoItemsList } = this.state;
+      const index = toDoItemsList.indexOf(item);
+      this.setState({
+        toDoItemsList: [
+          ...toDoItemsList.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete,
+          },
+          ...toDoItemsList.slice(index + 1),
+        ],
+      });
+    };
+  }
+  /* eslint-enable */
+
   render() {
+    const { toDoItemsList } = this.state;
     return (
       <div className="App">
-        {this.toDoItemsList.map((objItem, index) => (
-          <TodoItem key={index} item={objItem} />
-        ))}
+        {toDoItemsList.length > 0 &&
+          toDoItemsList.map((objItem, index) => (
+            <TodoItem
+              // eslint-disable-next-line react/no-array-index-key
+              key={`TodoItem${index}`}
+              item={objItem}
+              onclick={this.clickItem(objItem)}
+            />
+          ))}
+        {toDoItemsList.length === 0 && 'Nothing here'}
       </div>
     );
   }
