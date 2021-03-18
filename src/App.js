4eadc6228d4b70f79dd7dcedf45ad049e3060762
Toby <toby.nguyen.goldenowl@gmail.com>
2021-03-18 23:47:24 +0700

    feat:done feature filter all,active,completed

--- src/App.js
+++ src/App.js
@@ -1,55 +1,146 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './Components/Todoitem';
+import DownArrow from './Components/img/down-arrow.svg';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      // eslint-disable-next-line react/no-unused-state
+      newItem: '',
+      currentFilter: 'all', // all,active,complete
       toDoItemsList: [
-        { item: 'da bong', isComplete: true },
-        { item: 'xem phim', isComplete: true },
-        { item: 'choi the thao', isComplete: false },
+        { id: 1, item: 'da bong', isComplete: true },
+        { id: 2, item: 'xem phim', isComplete: true },
+        { id: 3, item: 'choi the thao', isComplete: false },
       ],
     };
+    this.onkeyup = this.onkeyup.bind(this);
+    this.onchange = this.onchange.bind(this);
   }
 
-  /* eslint-disable */
+  onkeyup(event) {
+    const { toDoItemsList } = this.state;
+    if (event.keyCode === 13) {
+      let text = event.target.value;
+      if (!text) {
+        return null;
+      }
+      text = text.trim();
+      if (!text) {
+        return null;
+      }
+      this.setState({
+        newItem: '',
+        toDoItemsList: [{ item: text, isComplete: false }, ...toDoItemsList],
+      });
+    }
+    return null;
+  }
+
+  onchange(event) {
+    this.setState({
+      newItem: event.target.value,
+    });
+  }
+
   clickItem(item) {
-    // this.toDoItemsList.isComplete = !this.toDoItemsList.isComplete;
-    return (_event) => {
-      const { isComplete } = item;
-      const { toDoItemsList } = this.state;
-      const index = toDoItemsList.indexOf(item);
+    const { toDoItemsList } = this.state;
+    const index = toDoItemsList.findIndex((toDos) => toDos.id === item.id);
+    if (index >= 0) {
       this.setState({
         toDoItemsList: [
           ...toDoItemsList.slice(0, index),
-          {
-            ...item,
-            isComplete: !isComplete,
-          },
+          item,
           ...toDoItemsList.slice(index + 1),
         ],
       });
-    };
+    }
+    return null;
   }
-  /* eslint-enable */
+
+  clickFilter(filter) {
+    const { toDoItemsList } = this.state;
+    const arrActive = [];
+    let check;
+    if (filter === 'active') {
+      check = false;
+    } else if (filter === 'completed') {
+      check = true;
+    } else {
+      this.setState({
+        currentFilter: filter,
+        toDoItemsList: [...toDoItemsList],
+      });
+    }
+    toDoItemsList.forEach((itemFilter) => {
+      if (itemFilter.isComplete === check) {
+        arrActive.push(itemFilter);
+      }
+    });
+    this.setState({
+      currentFilter: filter,
+    });
+  }
+
+  renderItem(objItem, index, currentFilter) {
+    return (
+      <TodoItem
+        key={index}
+        item={objItem}
+        currentFilter={currentFilter}
+        onclick={(item) => this.clickItem(item)}
+      />
+    );
+  }
 
   render() {
-    const { toDoItemsList } = this.state;
+    const { toDoItemsList, newItem, currentFilter } = this.state;
     return (
       <div className="App">
+        <div className="Header">
+          <img src={DownArrow} alt="down-arrow" width="32px" height="32px " />
+          <input
+            type="text"
+            placeholder="what needs to be done"
+            onKeyUp={this.onkeyup}
+            value={newItem}
+            onChange={this.onchange}
+          />
+        </div>
         {toDoItemsList.length > 0 &&
-          toDoItemsList.map((objItem, index) => (
-            <TodoItem
-              // eslint-disable-next-line react/no-array-index-key
-              key={`TodoItem${index}`}
-              item={objItem}
-              onclick={this.clickItem(objItem)}
-            />
-          ))}
+          toDoItemsList.map((objItem, index) => {
+            if (
+              currentFilter === 'active' &&
+              objItem &&
+              objItem.isComplete === false
+            ) {
+              return this.renderItem(objItem, index, currentFilter);
+            }
+            if (
+              currentFilter === 'completed' &&
+              objItem &&
+              objItem.isComplete === true
+            ) {
+              return this.renderItem(objItem, index, currentFilter);
+            }
+            if (currentFilter === 'all' && objItem) {
+              return this.renderItem(objItem, index, currentFilter);
+            }
+            return null;
+          })}
         {toDoItemsList.length === 0 && 'Nothing here'}
+        <div className="BtnOptionClick">
+          <button type="button" onClick={() => this.clickFilter('all')}>
+            All
+          </button>
+          <button type="button" onClick={() => this.clickFilter('active')}>
+            Active
+          </button>
+          <button type="button" onClick={() => this.clickFilter('completed')}>
+            Completed
+          </button>
+        </div>
       </div>
     );
   }
--- src/Components/Todoitem.js
+++ src/Components/Todoitem.js
@@ -1,21 +1,27 @@
-/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import './Todoitem.css';
 import classNames from 'classnames';
 class TodoItem extends Component {
   /* cách xử lý cho onclick khi không có context */
-  // constructor(props) {
-  //   super(props);
-  //   this.clickItem = this.clickItem.bind(this);
-  // }
-  /* eslint max-len: ["error", { "code": 500 }] */
-  render() {
+  constructor(props) {
+    super(props);
+    this.handleItem = this.handleItem.bind(this);
+  }
+
+  handleItem() {
     const { item, onclick } = this.props;
+    // eslint-disable-next-line no-unused-expressions
+    onclick && onclick({ ...item, isComplete: !item.isComplete });
+  }
+
+  render() {
+    const { item } = this.props;
     let url =
+      // eslint-disable-next-line max-len
       'data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23ededed%22%20stroke-width%3D%223%22/%3E%3C/svg%3E';
     if (item.isComplete) {
-      // eslint-disable-next-line no-const-assign
       url =
+        // eslint-disable-next-line max-len
         'data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23bddad5%22%20stroke-width%3D%223%22/%3E%3Cpath%20fill%3D%22%235dc2af%22%20d%3D%22M72%2025L42%2071%2027%2056l-4%204%2020%2020%2034-52z%22/%3E%3C/svg%3E';
     }
     return (
@@ -24,9 +30,8 @@ class TodoItem extends Component {
           'TodoItem-Complete': item.isComplete,
         })}
         aria-hidden="true"
-        // eslint-disable-next-line react/jsx-no-comment-textnodes
       >
-        <button type="button" onClick={onclick}>
+        <button type="button" onClick={this.handleItem}>
           <img src={url} alt="" width="40px" height="49px " />
         </button>
         <p>{item.item}</p>
